31a1e9bb020b735f65dc973653b1564cafb8433d
dannupn <imanueldannu910@gmail.com>
2025-11-21 10:03:12 +0700

    ganti isi index.php

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AWOKWAOK PHP</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
+    <h1>Halo, ini tes koneksi CSS!</h1>
 </body>
 
 </html>
